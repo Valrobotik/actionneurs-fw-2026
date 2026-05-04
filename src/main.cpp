bcbeb31e96a0fce750179808e949bbdf7cf80176
Rémi Ait-Younes <remi@ait-younes.fr>
2026-05-04 08:09:18 +0200

    add ros

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,14 @@
 #include <ESP32Servo.h>
 
 #include "pin_definitions.h"
-
-// put function declarations here:
-int myFunction(int, int);
+#include "ros.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  init_ros();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  ros_loop();
+  delay(50);
 }
