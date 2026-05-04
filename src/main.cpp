6c6d97db0b52c390449ecb6c76b910f24a36be4e
Rémi Ait-Younes <remi@ait-younes.fr>
2026-05-04 07:56:41 +0200

    add pin definitions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
+#include "pin_definitions.h"
+
 // put function declarations here:
 int myFunction(int, int);
 
---
+++ src/pin_definitions.h
@@ -0,0 +1,27 @@
+#pragma once
+
+// HW Rev 01 pin definitions
+#define SERVO_01_PIN 14
+#define SERVO_02_PIN 13
+#define SERVO_03_PIN 12
+#define SERVO_04_PIN 11
+#define SERVO_05_PIN 10
+#define SERVO_06_PIN 9
+#define SERVO_07_PIN 8
+#define SERVO_08_PIN 7
+#define SERVO_09_PIN 6
+#define SERVO_10_PIN 5
+#define SERVO_11_PIN 4
+#define SERVO_12_PIN 3
+#define SERVO_13_PIN 2
+#define SERVO_14_PIN 1
+
+#define CAN_RX_PIN 42
+#define CAN_TX_PIN 45
+
+#define RGB_LED_PIN 21
+
+#define ILM_A_PIN 17
+#define PG_A_PIN 18
+#define ILM_B_PIN 15
+#define PG_B_PIN 16
